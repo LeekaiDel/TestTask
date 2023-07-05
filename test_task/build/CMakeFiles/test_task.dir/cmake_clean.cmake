e42f85daec1be2475671f7f0f3083241b4cd1a16
Leekai <antondel00@gmail.com>
2023-07-05 22:54:12 +0300

    create repo

---
+++ test_task/build/CMakeFiles/test_task.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "../test_task_ru_RU.ts"
+  "CMakeFiles/test_task_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/test_task_autogen.dir/ParseCache.txt"
+  "test_task_autogen"
+  "CMakeFiles/test_task.dir/main.cpp.o"
+  "CMakeFiles/test_task.dir/main.cpp.o.d"
+  "CMakeFiles/test_task.dir/mainwindow.cpp.o"
+  "CMakeFiles/test_task.dir/mainwindow.cpp.o.d"
+  "CMakeFiles/test_task.dir/test_task_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/test_task.dir/test_task_autogen/mocs_compilation.cpp.o.d"
+  "test_task"
+  "test_task.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_task.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
